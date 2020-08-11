e321d2b570d4910a50a13f844f5caf6f459a43d2
Freddie <tsfreddiewang@gmail.com>
2020-08-11 23:44:44 +0100

    grep

--- samples/bpf/ndp_grep.c
+++ samples/bpf/ndp_grep.c
@@ -1,27 +1,54 @@
 #include <uapi/linux/bpf.h>
 #include <bpf/bpf_helpers.h>
 
+#define PATTERN "search_pattern"
+#define PATTERN_LEN 14
+
 SEC("nvme_ndpm")
 int module(struct nvme_ndp_context *ctx)
 {
-	int i;
+	unsigned int i, j, k, o = 0, b = 0;
 	char *in = ctx->in_data;
 	char *out = ctx->out_data;
 	unsigned int len = ctx->out_data_len;
-	
-	if (ctx->op) {
-		// WRITE
-		for (i = 0; i < len; i++) {
-			out[i] = 'K';
-		}
-	} else {
-		// READ
-		for (i = 0; i < 4096; i++) {
-			out[i] = i < 512 ? 'Y' : in[i];
-		}
+    char skipping = 0;
+
+    const char *pattern = PATTERN;
+    ctx->flag = 1;
+
+	for (i = 0; i < ctx->in_data_len; ++i) {
+		if (skipping) {
+            if (in[i] == '\n') {
+                skipping = 0;
+                for (k = b; k < i; ++k) {
+                    if (o >= len - 1) return 0;
+                    out[o++] = in[k];
+                }
+                if (o >= len - 1) return 0;
+                out[o++] = '\n';
+            }
+        } else {
+            skipping = 1;
+            for (j = 0; j < PATTERN_LEN; ++j) {
+                if (i+j >= ctx->in_data_len || in[i+j] != pattern[j]) {
+                    skipping = 0;
+                    break;
+                }
+            }
+        }
+        if (in[i] == '\n') {
+            b = i + 1;
+        }
 	}
 
-	ctx->flag = 1;
+    if (skipping) {
+        for (k = b; k < ctx->in_data_len; ++k) {
+            if (o >= len - 1) return 0;
+            out[o++] = in[k];
+        }
+        if (o >= len - 1) return 0;
+        out[o++] = '\n';
+    }
 	return 0;
 }
 char _license[] SEC("license") = "GPL";
--- samples/bpf/ndp_snappy.c
+++ samples/bpf/ndp_snappy.c
@@ -4,22 +4,6 @@
 SEC("nvme_ndpm")
 int module(struct nvme_ndp_context *ctx)
 {
-	int i;
-	char *in = ctx->in_data;
-	char *out = ctx->out_data;
-	unsigned int len = ctx->out_data_len;
-	
-	if (ctx->op) {
-		// WRITE
-		for (i = 0; i < len; i++) {
-			out[i] = 'K';
-		}
-	} else {
-		// READ
-		for (i = 0; i < 4096; i++) {
-			out[i] = i < 512 ? 'Y' : in[i];
-		}
-	}
 
 	ctx->flag = 1;
 	return 0;
